0a592f2a55edd7fd92fc5c81fd0436463525fa04
Suresh y <47291260+YSureshN@users.noreply.github.com>
2021-12-06 16:00:31 +0530

    s3-static-website-with-domain

--- main.tf
+++ main.tf
@@ -1,224 +1,119 @@
-provider "aws" {
-  region  = "us-east-1"
-}
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "my-tf-log-bucket123"
-  acl    = "log-delivery-write"
-}
-resource "aws_s3_bucket" "suresh" {
-  bucket = "my-bucket12345-suresh"
-  acl    = "private"
-
-  logging {
-    target_bucket = aws_s3_bucket.log_bucket.id
-    target_prefix = "log/"
-  }
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-EOF
-  }
+resource "aws_s3_bucket" "website" {
+  bucket        = var.bucket_name
+  acl           = "private"
+  force_destroy = true
+  tags          = local.tags
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    id      = "log"
-    enabled = true
-
-    prefix = "log/"
-
-    tags = {
-      rule      = "log"
-      autoclean = "true"
-    }
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-
-    expiration {
-      days = 90
-    }
-  }
-
-  lifecycle_rule {
-    id      = "tmp"
-    prefix  = "tmp/"
-    enabled = true
-
-    expiration {
-      date = "2016-01-12"
-    }
-  }
 }
 
-resource "aws_s3_bucket_policy" "suresh" {
-  bucket = aws_s3_bucket.suresh.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression's result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "MYBUCKETPOLICY"
-    Statement = [
-      {
-        Sid       = "IPAllow"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource = "${aws_s3_bucket.suresh.arn}/*"
-        Condition = {
-          NotIpAddress = {
-            "aws:SourceIp" = "192.168.4.5/32"
-          }
-        }
-      },
-    ]
-  })
-  }
-locals {
-  s3_origin_id = "E3U13P6C1XV15R"
+resource "aws_s3_bucket_public_access_block" "s3block" {
+  bucket                  = aws_s3_bucket.website.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.suresh.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
-
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-    }
-  }
-
+resource "aws_cloudfront_distribution" "cf" {
   enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Some comment"
+  aliases             = [var.endpoint]
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "my-tf-log-bucket123"
-    prefix          = "myprefix"
-  }
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.website.bucket_regional_domain_name
 
-  aliases = ["mysite.example.com", "yoursite.example.com"]
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
 
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id       = aws_s3_bucket.website.bucket_regional_domain_name
     viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
 
     forwarded_values {
-      query_string = false
+      headers      = []
+      query_string = true
 
       cookies {
-        forward = "none"
+        forward = "all"
       }
     }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
   }
-
-  price_class = "PriceClass_200"
 
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      restriction_type = "none"
     }
   }
 
-  tags = {
-    Environment = "production"
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+  tags = local.tags
 }
 
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for ${var.endpoint}"
+}
 
+resource "aws_s3_bucket_policy" "s3policy" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.s3policy.json
+}
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
+resource "aws_acm_certificate" "cert" {
+  provider                  = aws.us-east-1
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method         = "DNS"
+  tags                      = local.tags
+}
+
+resource "aws_route53_record" "certvalidation" {
+  for_each = {
+    for d in aws_acm_certificate.cert.domain_validation_options : d.domain_name => {
+      name   = d.resource_record_name
+      record = d.resource_record_value
+      type   = d.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.domain.zone_id
+}
+
+resource "aws_acm_certificate_validation" "certvalidation" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for r in aws_route53_record.certvalidation : r.fqdn]
+}
+
+resource "aws_route53_record" "websiteurl" {
+  name    = var.endpoint
+  zone_id = data.aws_route53_zone.domain.zone_id
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cf.domain_name
+    zone_id                = aws_cloudfront_distribution.cf.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "bucket_name" {
+  # description = "S3 bucket name"
+  # type        = string
+  default = ["sureshdemohuexdemo2345"]
+}
+
+variable "endpoint" {
+  # description = "Endpoint url"
+  # type        = string
+  default = ["sureshdemo"]
+}
+
+variable "region" {
+  # description = "AWS region"
+  # type        = string
+  default = ["us-east-1"]
+}
+
+variable "domain_name" {
+  # description = "Domain name"
+  # type        = string
+  default = ["sureshHuex"]
+}
